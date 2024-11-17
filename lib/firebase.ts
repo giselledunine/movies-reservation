d95b1ef6d1469bcbd7bce1dac99f169ceeb3ae76
giselledunine <sophia.hmamouche@outlook.com>
2024-11-17 15:19:55 +0100

    fix: turned to environement ariables sensible infos

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -10,9 +10,9 @@ const firebaseConfig = {
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
     databaseURL: process.env.FIREBASE_DATABASE_URL,
     projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: "random-90ee8.appspot.com",
-    messagingSenderId: "949868149413",
-    appId: "1:949868149413:web:5d2c7c2d38e791f723ab4c",
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
