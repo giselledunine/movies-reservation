4269f52bb8f51a9662cd401da963fedb7b986fbc
giselledunine <sophia.hmamouche@outlook.com>
2024-11-18 20:29:07 +0100

    fix: fixed session fetching

--- components/MenuBar.tsx
+++ components/MenuBar.tsx
@@ -2,7 +2,6 @@
 
 import { useRouter } from "next/navigation";
 import { usePathname } from "next/navigation";
-import { UserWithRoleSession } from "@/lib/authOptions";
 import Image from "next/image";
 import {
     DropdownMenu,
@@ -20,25 +19,15 @@ import { AvatarIcon } from "@/components/AvatarIcon";
 
 import { signOut, useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
-import { Skeleton } from "./ui/skeleton";
-import { useUserStore } from "@/stores/userStore";
+
 import { DropdownMenuGroup } from "@radix-ui/react-dropdown-menu";
 
 export default function MenuBar() {
     const router = useRouter();
     const pathname = usePathname();
-    const storeSession = useUserStore((state) => state.session);
-    const { setSession } = useUserStore((state) => state);
     const session = useSession();
+    console.log("session", session);
     const [isTop, setIsTop] = useState<boolean>(true);
-    const sessionWithRole = storeSession as UserWithRoleSession;
-
-    useEffect(() => {
-        if (session.data && !storeSession) {
-            setSession(session.data as UserWithRoleSession);
-        }
-    }),
-        [session];
 
     useEffect(() => {
         const handleScroll = () => {
@@ -48,19 +37,21 @@ export default function MenuBar() {
                 setIsTop(false);
             }
         };
-        document.addEventListener("scroll", handleScroll);
-        return window.removeEventListener("scroll", handleScroll);
-    }),
-        [];
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
 
     return (
         <div className="flex bg-background z-50 items-center justify-between w-full p-4 fixed top-0 left-0 z-0">
             <div className="flex items-center gap-4 w-full justify-between">
                 <div>
-                    {storeSession ? (
+                    {session.status === "authenticated" ? (
                         <DropdownMenu>
                             <DropdownMenuTrigger>
-                                <AvatarIcon session={sessionWithRole} />
+                                <AvatarIcon session={session} />
                             </DropdownMenuTrigger>
                             <DropdownMenuContent className="w-56 mx-4">
                                 <DropdownMenuLabel>
@@ -83,10 +74,8 @@ export default function MenuBar() {
                                         Mes réservations
                                     </DropdownMenuItem>
                                 </DropdownMenuGroup>
-
                                 <DropdownMenuGroup>
-                                    {sessionWithRole?.user?.role ===
-                                        "admin" && (
+                                    {session?.data?.user?.role === "admin" && (
                                         <>
                                             <DropdownMenuSeparator />
                                             <DropdownMenuLabel>
@@ -135,8 +124,6 @@ export default function MenuBar() {
                                 </DropdownMenuItem>
                             </DropdownMenuContent>
                         </DropdownMenu>
-                    ) : session.status === "loading" ? (
-                        <Skeleton className="h-10 w-10 rounded-full" />
                     ) : (
                         <>
                             <Button
